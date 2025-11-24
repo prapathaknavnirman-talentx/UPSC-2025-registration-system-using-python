843f9b57fb8683e84ee46cf6700140501e9e392d
PRANAV PATHAK <pranavpathakscsr@gmail.com>
2025-11-24 13:24:33 +0530

    Delete Login_info.sql

--- Login_info.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS login_info (
-    username VARCHAR(50),
-    password VARCHAR(50)
-);
-
--- Optional: insert a default login
-INSERT INTO login_info (username, password) VALUES ('admin', 'admin123');
